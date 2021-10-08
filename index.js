c113788a60c68d233ce8e8678f2e6b274613895b
Loc <hoanglocst900@gmail.com>
2021-10-08 18:57:35 +0700

    DELETE - restart project

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const winston = require('winston');
-
-require('./startup/routes')(app);
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => winston.info(`Listening on port ${port}...`));
