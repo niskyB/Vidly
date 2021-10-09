857ba0fafd2f2f882f4c5e0ac18ea7cfac81ad2a
Loc <hoanglocst900@gmail.com>
2021-10-09 20:19:40 +0700

    UPDATE - update all methods

--- routes/movies.js
+++ routes/movies.js
@@ -1,67 +1,67 @@
 const express = require('express');
 const router = express.Router();
 const { connect } = require('../utils/dbConnector');
-const { Genre } = require('../models/genre');
+const { Movie } = require('../models/movie');
 const { v4: uuidv4 } = require('uuid');
 
 router.get('/', async(req, res) => {
-    connect(req, res, 'SELECT * FROM ??', [Genre.dbName],
+    connect(req, res, 'SELECT * FROM ??', [Movie.dbName],
         function(error, results, fields) {
             if (error) res.status(500).send('Something went wrong.');
             else {
-                if (results[0] === undefined) res.status(404).send('The genre list is empty.');
+                if (results[0] === undefined) res.status(404).send('The movie list is empty.');
                 else res.send(results);
             }
         });
 });
 
 router.get('/:id', async(req, res) => {
-    connect(req, res, 'SELECT * FROM ?? WHERE ?? = ?', [Genre.dbName, "genreId", req.params.id],
+    connect(req, res, 'SELECT * FROM ?? WHERE ?? = ?', [Movie.dbName, "movieId", req.params.id],
         function(error, results, fields) {
             if (error) res.status(500).send('Something went wrong.');
             else {
-                if (results[0] === undefined) res.status(404).send('The genre with the given ID was not found.');
+                if (results[0] === undefined) res.status(404).send('The movie with the given ID was not found.');
                 else res.send(results[0]);
             }
         });
 });
 
 router.post('/', async(req, res) => {
-    const error = Genre.validateGenre(req.body);
+    const error = Movie.validateMovie(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const genreId = uuidv4().substr(1, Genre.idLength);
-    connect(req, res, "INSERT INTO ??(genreId, genreName) VALUES(?, ?)", [Genre.dbName, genreId, req.body.genreName],
+    const movieId = uuidv4().substr(1, Movie.idLength);
+    connect(req, res, "INSERT INTO ??(movieId, title, numberInStock, dailyRentalRate, genreId) VALUES(?, ?, ?, ?, ?)", [Movie.dbName, movieId, req.body.title, req.body.numberInStock, req.body.dailyRentalRate, req.body.genreId],
         function(error, results, fields) {
             if (error) res.status(500).send('Something went wrong.');
             else {
                 if (results.affectedRows === 0) res.status(500).send('Something was wrong.');
-                else res.send('Add genre successful.');
+                else res.send('Add movie successful.');
             }
         });
 });
 
 router.put('/:id', async(req, res) => {
-    const error = Genre.validateGenre(req.body);
+    const error = Movie.validateMovie(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    connect(req, res, "UPDATE ?? SET genreName = ? WHERE genreId = ?", [Genre.dbName, req.body.genreName, req.params.id],
+    connect(req, res, "UPDATE ?? SET title = ?, numberInStock = ?, dailyRentalRate = ?, genreId = ? WHERE movieId = ?", [Movie.dbName, req.body.title, req.body.numberInStock, req.body.dailyRentalRate, req.body.genreId, req.params.id],
         function(error, results, fields) {
             if (error) res.status(500).send('Something went wrong.');
             else {
-                if (results.affectedRows === 0) res.status(404).send('The genre with the given ID was not found.');
-                else res.send('Update genre successful.');
+                if (results.affectedRows === 0) res.status(404).send('The movie with the given ID was not found.');
+                else res.send('Update movie successful.');
             }
         });
 });
 
 router.delete('/:id', async(req, res) => {
-    connect(req, res, "DELETE FROM ?? WHERE genreId = ?", [Genre.dbName, req.params.id],
+    connect(req, res, "DELETE FROM ?? WHERE movieId = ?", [Movie.dbName, req.params.id],
         function(error, results, fields) {
             if (error) res.status(500).send('Something went wrong.');
             else {
-                if (results.affectedRows === 0) res.status(404).send('The genre with the given ID was not found.');
-                else res.send('Delete genre successful.');
+                if (results.affectedRows === 0) res.status(404).send('The movie with the given ID was not found.');
+                else res.send('Delete movie successful.');
             }
         });
 });
