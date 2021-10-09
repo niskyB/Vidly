aaa5b4b336526dca392f3fc2ab6e0c6b96973b4f
Loc <hoanglocst900@gmail.com>
2021-10-09 19:26:59 +0700

    UPDATE - add genres route

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const customers = require('../routes/customers');
+const genres = require('../routes/genres');
 
 module.exports = function(app) {
     app.use(express.json());
     app.use('/api/customers', customers);
+    app.use('/api/genres', genres);
 }
