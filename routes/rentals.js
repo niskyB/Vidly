2b40905961cddcbe8fcdecc9f05e4467a2817aab
Loc <hoanglocst900@gmail.com>
2021-10-09 20:58:35 +0700

    UPDATE - fix name of Rental

--- routes/rentals.js
+++ routes/rentals.js
@@ -5,22 +5,22 @@ const { Rental } = require('../models/rental');
 const { v4: uuidv4 } = require('uuid');
 
 router.get('/', async(req, res) => {
-    connect(req, res, 'SELECT * FROM ??', [Genre.dbName],
+    connect(req, res, 'SELECT * FROM ??', [Rental.dbName],
         function(error, results, fields) {
             if (error) res.status(500).send('Something went wrong.');
             else {
-                if (results[0] === undefined) res.status(404).send('The genre list is empty.');
+                if (results[0] === undefined) res.status(404).send('The rental list is empty.');
                 else res.send(results);
             }
         });
 });
 
 router.get('/:id', async(req, res) => {
-    connect(req, res, 'SELECT * FROM ?? WHERE ?? = ?', [Genre.dbName, "genreId", req.params.id],
+    connect(req, res, 'SELECT * FROM ?? WHERE ?? = ?', [Rental.dbName, "rentalId", req.params.id],
         function(error, results, fields) {
             if (error) res.status(500).send('Something went wrong.');
             else {
-                if (results[0] === undefined) res.status(404).send('The genre with the given ID was not found.');
+                if (results[0] === undefined) res.status(404).send('The rental with the given ID was not found.');
                 else res.send(results[0]);
             }
         });
@@ -30,8 +30,8 @@ router.post('/', async(req, res) => {
     const error = Genre.validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const genreId = uuidv4().substr(1, Genre.idLength);
-    connect(req, res, "INSERT INTO ??(genreId, genreName) VALUES(?, ?)", [Genre.dbName, genreId, req.body.genreName],
+    const rentalId = uuidv4().substr(1, Rental.idLength);
+    connect(req, res, "INSERT INTO ??(rentalId, genreName) VALUES(?, ?)", [Genre.dbName, genreId, req.body.genreName],
         function(error, results, fields) {
             if (error) res.status(500).send('Something went wrong.');
             else {
