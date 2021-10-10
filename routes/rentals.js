00e52361e076b88ac6cdaec7b76b0ec2f4eccb59
Loc <hoanglocst900@gmail.com>
2021-10-10 19:05:07 +0700

    UPDATE - add transaction

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { connect } = require('../utils/dbConnector');
+const { connect, pool, promisePool } = require('../utils/dbConnector');
 const { Rental } = require('../models/rental');
+const { Customer } = require('../models/customer');
+const { Movie } = require('../models/movie');
 const { v4: uuidv4 } = require('uuid');
 
 router.get('/', async(req, res) => {
@@ -27,18 +29,58 @@ router.get('/:id', async(req, res) => {
 });
 
 router.post('/', async(req, res) => {
-    const error = Genre.validateGenre(req.body);
+    const error = Rental.validateRental(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const rentalId = uuidv4().substr(1, Rental.idLength);
-    connect(req, res, "INSERT INTO ??(rentalId, genreName) VALUES(?, ?)", [Genre.dbName, genreId, req.body.genreName],
-        function(error, results, fields) {
-            if (error) res.status(500).send('Something went wrong.');
+    pool.getConnection(async function(err, connection) {
+        if (err) res.status(500).send('Something went wrong.');
+        // Use the connection
+        else {
+            let results = await promisePool.query(`SELECT * FROM ?? WHERE ?? = ?`, [Customer.dbName, "customerId", req.body.customerId]);
+            if (results[0].length === 0) res.status(404).send('Invalid customer.');
             else {
-                if (results.affectedRows === 0) res.status(500).send('Something was wrong.');
-                else res.send('Add genre successful.');
+                results = await promisePool.query('SELECT * FROM ?? WHERE ?? = ?', [Movie.dbName, "movieId", req.body.movieId]);
+                if (results[0].length === 0) res.status(404).send('Invalid movie.');
+                else if (results[0][0].numberInStock === 0) res.status(400).send('Movie is not in stock.');
+                else {
+                    connection.beginTransaction(function(err) {
+                        if (err) res.status(500).send('Something went wrong.');
+                        else {
+                            connection.query('UPDATE ?? SET numberInStock = ? WHERE movieId = ?', [Movie.dbName, results[0][0].numberInStock - 1, req.body.movieId],
+                                function(error, results, fields) {
+                                    if (error) {
+                                        return connection.rollback(function() {
+                                            throw error;
+                                        });
+                                    }
+
+                                    const log = 'Post ' + results.insertId + ' added';
+                                    const rentalId = uuidv4().substr(1, Movie.idLength);
+
+                                    connection.query('INSERT INTO ??(rentalId, customerId, movieId) VALUES(?, ?, ?)', [Rental.dbName, rentalId, req.body.customerId, req.body.movieId],
+                                        function(error, results, fields) {
+                                            if (error) {
+                                                return connection.rollback(function() {
+                                                    throw error;
+                                                });
+                                            }
+                                            connection.commit(function(err) {
+                                                if (err) {
+                                                    return connection.rollback(function() {
+                                                        throw err;
+                                                    });
+                                                }
+                                                res.send('Add rental successful!');
+                                            });
+                                        });
+                                });
+                        }
+                    });
+                }
             }
-        });
+        }
+        connection.release();
+    });
 });
 
 router.put('/:id', async(req, res) => {
