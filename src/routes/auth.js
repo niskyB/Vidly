9489454dcdd7e05eff5879da04c840906155a25d
Loc <hoanglocst900@gmail.com>
2021-10-12 21:31:58 +0700

    UPDATE - add \n

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -10,15 +10,19 @@ router.post('/', async(req, res) => {
     // check request body
     const error = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
+
     // find email in database
     let results = await promisePool.query('SELECT * FROM ?? WHERE email = ?', [User.dbName, req.body.email]);
     if (results[0].length === 0) return res.status(400).send('Ivalid email or password.');
+
     // check password
     const user = new User(results[0][0].userId, results[0][0].username, req.body.email, results[0][0].password, results[0][0].isAdmin);
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Ivalid email or password.');
+
     // generate auth token
     const token = user.generateAuthToken();
+
     // respone token 
     res.send(token);
 });
