08e421c0355b44e812f90834d70d4eadfe705da9
Loc <hoanglocst900@gmail.com>
2021-10-12 16:50:16 +0700

    UPDATE - add config and logging file

---
+++ startup/logging.js
@@ -0,0 +1,16 @@
+const winston = require('winston');
+require('express-async-errors');
+
+module.exports = function() {
+    process.on('uncaughtException', (ex) => {
+        winston.error(ex.message, ex);
+        process.exit(1);
+    })
+
+    process.on('unhandledRejection', (ex) => {
+        winston.error(ex.message, ex);
+        process.exit(1);
+    })
+
+    winston.add(new winston.transports.File({ filename: 'logfile.log', useUnifiedTopology: true }));
+}
