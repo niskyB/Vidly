b0e2491f8ff51870e3ed44904ab1b3e338decb8e
Loc <hoanglocst900@gmail.com>
2021-10-12 16:39:57 +0700

    UPDATE - use error middleware

--- startup/routes.js
+++ startup/routes.js
@@ -5,6 +5,7 @@ const movies = require('../routes/movies');
 const rentals = require('../routes/rentals');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
+const error = require('../middleware/error');
 
 module.exports = function(app) {
     app.use(express.json());
@@ -14,4 +15,5 @@ module.exports = function(app) {
     app.use('/api/rentals', rentals);
     app.use('/api/users', users);
     app.use('/api/auth', auth);
+    app.use(error);
 }
