361080a2ff33505aaa9901384c6f912870e80fcd
Loc <hoanglocst900@gmail.com>
2021-10-16 22:06:10 +0700

    UPDATE - refactor variable name

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,7 +1,9 @@
 function getResponseForm(data, error, message) {
+    const errorObject = error.details[0];
+
     return {
         data: data,
-        [error.details[0].context.label]: error.details[0].message.replace("\"" + error.details[0].context.label + "\" ", "").replace("must", "should"),
+        [errorObject.context.label]: errorObject.message.replace("\"" + errorObject.context.label + "\" ", "").replace("must", "should"),
         message: message
     };
 }
