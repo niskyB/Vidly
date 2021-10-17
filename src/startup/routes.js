b76dc40d1c8d86651389b3793bece64566cae7f7
Loc <hoanglocst900@gmail.com>
2021-10-17 15:25:24 +0700

    UPDATE - use cookieParser

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -6,9 +6,11 @@ const rentals = require('../routes/rentals');
 const users = require('../routes/users');
 const auth = require('../routes/auth');
 const error = require('../middleware/error');
+const cookieParser = require('cookie-parser')
 
 module.exports = function(app) {
     app.use(express.json());
+    app.use(cookieParser());
     app.use('/api/customers', customers);
     app.use('/api/genres', genres);
     app.use('/api/movies', movies);
