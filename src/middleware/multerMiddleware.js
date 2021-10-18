a60b5c5ca62ccd6077854cd1afd19a8c3bf10195
Loc <hoanglocst900@gmail.com>
2021-10-18 21:08:12 +0700

    ADD - add multer error middleware

---
+++ src/middleware/multerMiddleware.js
@@ -0,0 +1,15 @@
+const status = require('../constants/status');
+const multer = require('multer');
+
+exports = function multerErrorMiddleware(multerAction) {
+    return (req, res, next) => {
+        multerAction(req, res, err => {
+            if (err instanceof multer.MulterError) {
+                return res.status(status.BAD_REQUEST).send(err.message);
+            } else if (err) {
+                return res.status(status.BAD_REQUEST).send(err);
+            }
+            next();
+        });
+    };
+}
